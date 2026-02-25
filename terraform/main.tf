618461f2eedb156ed458f6c8702f8d9d7897759c
Ahmad Amin <ahmad167471.com>
2026-02-25 22:53:23 +0530

    Update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,34 +1,38 @@
 terraform {
-    backend "s3" {
+  backend "s3" {
     bucket  = "ahmad-terraform-state-2026"
     key     = "bluegreen/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
   }
 }
+
 module "vpc" {
-  source       = "./modules/vpc"
+  source       = ".terraform/modules/vpc"
   project_name = var.project_name
 }
 
 module "security_groups" {
-  source       = "./modules/security_groups"
+  source       = ".terraform/modules/security_groups"
   vpc_id       = module.vpc.vpc_id
   project_name = var.project_name
 }
+
 module "alb" {
-  source         = "./modules/alb"
+  source         = ".terraform/modules/alb"
   vpc_id         = module.vpc.vpc_id
   public_subnets = module.vpc.public_subnet_ids
   alb_sg_id      = module.security_groups.alb_sg_id
   project_name   = var.project_name
 }
+
 module "ecr" {
-  source          = "./modules/ecr"
+  source          = ".terraform/modules/ecr"
   repository_name = "ahmad-strapi-bluegreen"
 }
+
 module "rds" {
-  source = "./modules/rds"
+  source = ".terraform/modules/rds"
 
   project_name    = var.project_name
   vpc_id          = module.vpc.vpc_id
@@ -40,8 +44,9 @@ module "rds" {
   db_username = var.db_username
   db_password = var.db_password
 }
+
 module "ecs" {
-  source          = "./modules/ecs"
+  source          = ".terraform/modules/ecs"
   private_subnets = module.vpc.private_subnets
   ecs_sg_id       = module.security_groups.ecs_sg_id
   blue_tg_arn     = module.alb.blue_tg_arn
@@ -51,8 +56,9 @@ module "ecs" {
   db_username = var.db_username
   db_password = var.db_password
 }
+
 module "codedeploy" {
-  source        = "./modules/codedeploy"
+  source        = ".terraform/modules/codedeploy"
   cluster_name  = module.ecs.cluster_name
   service_name  = module.ecs.service_name
   blue_tg_name  = module.alb.blue_tg_name
