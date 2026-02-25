a8c7320761d32ad4654c9130d8c85ff05b662043
Ahmad Amin <ahmad167471.com>
2026-02-26 03:04:01 +0530

    Fix ECS and VPC modules

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -1,31 +1,3 @@
-variable "project_name" {
-  type = string
-}
-
-variable "private_subnets" {
-  type = list(string)
-}
-
-variable "ecs_sg_id" {
-  type = string
-}
-
-variable "blue_tg_arn" {
-  type = string
-}
-
-variable "db_host" {
-  type = string
-}
-
-variable "db_username" {
-  type = string
-}
-
-variable "db_password" {
-  type = string
-}
-
 #########################
 # ECS Cluster
 #########################
