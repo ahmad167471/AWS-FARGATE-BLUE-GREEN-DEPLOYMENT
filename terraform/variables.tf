46aafe621894ef15d96854da6644c7e4d2463666
Ahmad Amin <ahmad167471.com>
2026-02-26 07:38:30 +0530

    terraform/variables.tf fixed

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,19 +1,23 @@
-variable "region" {
-  default = "us-east-1"
+variable "aws_region" {
+  description = "AWS region for all resources"
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "project_name" {
-  default = "ahmad-bluegreen"
+  description = "The name of the project, used for naming resources"
+  type        = string
+  default     = "ahmad-bluegreen"
 }
 
 variable "db_username" { 
   description = "RDS PostgreSQL username" 
-  type = string 
-  default = "postgres" 
+  type        = string 
+  default     = "postgres" 
 }
 
 variable "db_password" { 
   description = "RDS PostgreSQL password" 
-  type = string 
-  sensitive = true 
+  type        = string 
+  sensitive   = true 
 }
